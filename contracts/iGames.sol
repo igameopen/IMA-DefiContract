43c687482a994ed9edc58c7afd9dda52fde77f24
Raindrop <h_villiger@163.com>
2023-05-22 17:10:52 +0800

    feat: 删除白名单; UniswapV3Pool可更改;

--- contracts/iGames.sol
+++ contracts/iGames.sol
@@ -11,21 +11,17 @@ import "./library/Percentages.sol";
 contract iGames is ERC20, Ownable {
     using Percentages for uint256;
     using SafeMath for uint256;
-
-    // Goerli
-    address public constant WETH9 = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
-    address public constant FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+    
     address public constant POSITION_MANAGER = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
 
-    address public constant MARKETING = 0xebdeA78F37588752AFED1681C8E068F23CFEc010;
-    address public constant LP = 0xCCF5B6C077996ba12f63b283c5c8335ADCdfEe66;
+    address public constant MARKETING = 0xb728c15C35ADF40A8627a6dfA2614D8E84f03361;
+    address public constant LP = 0x0d8ff05d9F10b3D3eC32E38bC03f0571B07B403b;
 
-    address public immutable _uniswapV3Pool;
+    address public _uniswapV3Pool;
 
-    uint16 public _marketingFee = 60;
-    uint16 public _lpFee = 330;
-
-    mapping (address => bool) private _excludedFee;
+    uint16 public _marketingFee = 50;
+    uint16 public _lpFee = 200;
+    bool public _hasFee = true;
 
     modifier percentage(uint16 val) {
         require(val <= 10000, "out of limit");
@@ -34,29 +30,15 @@ contract iGames is ERC20, Ownable {
 
     constructor() ERC20("iGames", "IGS") {
         address thisAddr = address(this);
-        _mint(thisAddr, 840000 * 10 ** 18);
-
-        _uniswapV3Pool = IUniswapV3Factory(FACTORY).createPool(WETH9, thisAddr, 3000);
-        IUniswapV3Pool(_uniswapV3Pool).initialize(1000);
-
-        _excludedFee[thisAddr] = true;
-        _excludedFee[MARKETING] = true;
-        _excludedFee[LP] = true;
-        _excludedFee[msg.sender] = true;
+        _mint(thisAddr, 1_300_000_000 * 10 ** 18);
     } 
 
     function mintReward(address miner, uint256 amount) external onlyOwner {
         super._transfer(address(this), miner, amount);
     }
 
-    function addExcludedFee(address addr) external onlyOwner {
-        require(address(0) != addr, "ExcludedFee: address is zero");
-        _excludedFee[addr] = true;
-    }
-
-    function removeExcludedFee(address addr) external onlyOwner {
-        require(address(0) != addr, "ExcludedFee: address is zero");
-        _excludedFee[addr] = false;
+    function setHasFee(bool hasFee) external onlyOwner {
+        _hasFee = hasFee;
     }
 
     function setLPFee(uint16 lpFee) external onlyOwner percentage(lpFee) {
@@ -67,12 +49,16 @@ contract iGames is ERC20, Ownable {
         _marketingFee = marketingFee;
     }
 
+    function setUniswapV3Pool(address uniswapV3Pool) external onlyOwner {
+        _uniswapV3Pool = uniswapV3Pool;
+    }
+
     function _transfer(
         address from,
         address to,
         uint256 amount
     ) internal override {
-        if (!_isSwapTransfer(from, to) || _excludedFee[from] || _excludedFee[to]) {
+        if (!_isSwapTransfer(from, to) || _uniswapV3Pool == address(0) || !_hasFee) {
             super._transfer(from, to, amount);
             return;    
         }
@@ -84,12 +70,6 @@ contract iGames is ERC20, Ownable {
         super._transfer(from, LP, lpFee);
         super._transfer(from, MARKETING, marketingFee);
         super._transfer(from, to, trunAmount);
-    }
-
-    function _transferOwnership(address newOwner) internal override {
-        super._transferOwnership(newOwner);
-        _excludedFee[msg.sender] = false;
-        _excludedFee[newOwner] = true;
     }
 
     function _isSwapTransfer(address from, address to) private view returns (bool) {
